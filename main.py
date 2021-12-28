bc394f1f4e6999a9bc57d14de1674645af48e374
Leon Van Dyk <t0rus1@users.noreply.github.com>
2021-12-28 13:10:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('Our first Streamlit App')
+st.subheader('Introducing Streamlit in Automate Everything with Python')
+st.write('''This is our 
+first Web App
+Enjoy it
+''')
+
