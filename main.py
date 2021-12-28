3efe5b81611c81999eba78d243592e71b73a0e33
Leon Van Dyk <t0rus1@users.noreply.github.com>
2021-12-28 13:25:16 +0000

    add data table and chart

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+  'Series_1': [1,3,2,4,8,5],
+  'Series_2': [12,33,12,74,38,55]
+}
+
+df = pd.DataFrame(data)
+
 
 st.title('Our first Streamlit App')
 st.subheader('Introducing Streamlit in Automate Everything with Python')
@@ -7,3 +16,5 @@ first Web App
 Enjoy it
 ''')
 
+st.write(df)
+st.line_chart(df)
